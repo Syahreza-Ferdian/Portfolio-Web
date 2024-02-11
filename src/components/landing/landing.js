c9bf53cf9e8705d2ad0d473a55b3e7fb30011bc9
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-02-11 11:14:29 +0700

    Update

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -8,7 +8,7 @@ function Landing() {
             <div className="container h-100">
                 <div className="d-flex justify-content-center align-items-center vh-100">
                     <div className="wrapper text-center">
-                        <p className="upper-text">Hello! I'm</p>
+                        <p className="upper-text">Hello, I'm</p>
                         <p className="name-text">Syahreza Fisti Ferdian</p>
                         <small className="desc-land">Undergraduate Student of Information Technology</small>
                     </div>
