cb080622c7c7728ce759951e2c5323dde4f42ebf
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-08-27 07:39:28 +0700

    feat: add navbar and about section import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Landing from '../src/components/landing/landing.js';
-
+import Navbar from './components/navbar/navbar.js';
+import About from './components/about/about.js';
 
 function App() {
     return (
         <>
+            <Navbar />
+            <About />
             <Landing />
         </>
     );
